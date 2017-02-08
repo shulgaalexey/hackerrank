18127c0733eb36f6774ef11a21e7a85afe9b415c
shulga <a.shulga@samsung.com>
2017-02-09 08:52:15 +0900

    Code cleanup for Even Tree

--- even_tree.cpp
+++ even_tree.cpp
@@ -7,31 +7,31 @@ int main(void)
     int N = 0;
     int M = 0;
     cin >> N >> M;
-    
+
     vector<int> nodes(N);
     for (int i = 0; i < N; i++)
         nodes[i] = 0;
-    
+
     vector<pair<int, int> > edges(M);
     for (int i = 0; i < M; i++) {
         cin >> edges[i].first >> edges[i].second;
         edges[i].first--;
         edges[i].second--;
     }
-    
+
     for (int i = int(edges.size()) - 1; i >= 0; i--) {
         pair<int, int> e = edges[i];
         nodes[e.first]++;
         nodes[e.second] += nodes[e.first];
     }
-    
+
     int cuts = 0;
     for (int i = 1; i < N; i++) {
         if ((nodes[i] % 2) == 0)
             cuts++;
     }
-    
+
     cout << cuts << endl;
-    
+
     return 0;
 }
