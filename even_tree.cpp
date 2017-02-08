7ab3e478b2e0892270efeb8d0a932ca6aa8b2081
shulgaalexey <shulgaalexey@gmail.com>
2017-02-08 22:27:08 +0900

    Fixed Even Tree

--- even_tree.cpp
+++ even_tree.cpp
@@ -7,7 +7,25 @@ public:
 	vector<node *> children;
 	node *parent;
 	int rank;
+	int id;
+	node() : parent(NULL), rank(0), id(0) {}
 };
+
+void trace(const vector<node *> &tree)
+{
+	return;
+	cout << "Tracing:" << endl;
+	for (size_t i = 0; i < tree.size(); i++) {
+		node *n = tree[i];
+		cout << "<id: " << n->id << " rank: " <<  n->rank;
+		cout << " children: " << n->children.size() << ": [";
+		for (size_t j = 0; j < n->children.size(); j++) {
+			node *c = n->children[j];
+			cout << int(c->id) << " ";
+		}
+		cout << "]> " << endl;
+	}
+}
 
 int calc_rank(node *root)
 {
@@ -24,7 +42,7 @@ void raise_decreas(node *root, int rank)
 {
 	if (!root)
 		return;
-	root->rank += rank;
+	root->rank -= rank;
 	raise_decreas(root->parent, root->rank);
 }
 
@@ -37,9 +55,12 @@ int count_cuts(node *root)
 	for (size_t i = 0; i < root->children.size(); i++)
 		cnt += count_cuts(root->children[i]);
 
-	if (!root->rank & 1) {
-		cnt++;
-		raise_decreas(root->parent, root->rank);
+	if (!(root->rank & 1)) {
+		//cout << "CUTTING: id=" << root->id << " rank =" << root->rank << endl;
+		if (root->parent) {
+			cnt++;
+			raise_decreas(root->parent, root->rank);
+		}
 	}
 
 	return cnt;
@@ -51,26 +72,45 @@ int main(void)
 	int M = 0;
 	cin >> N >> M;
 
-	node *tree = new node[N];
+	vector<node *> tree(N);
 	for (int i = 0; i < N; i++) {
-		tree[i].rank = 0;
-		tree[i].parent = NULL;
+		tree[i] = new node;
+		tree[i]->rank = 0;
+		tree[i]->parent = NULL;
+		tree[i]->id = i;
 	}
+
+	vector<pair<int, int> > edges(M);
 
 	for (int i = 0; i < M; i++) {
 		int from = 0;
 		int to = 0;
 		cin >> from >> to;
-		tree[from - 1].parent = &tree[to - 1];
-		tree[to - 1].children.push_back(&tree[from - 1]);
+		edges[i].first = from - 1;
+		edges[i].second = to - 1;
 	}
 
-	calc_rank(&tree[0]);
+	for (int i = 0; i < M; i++) {
+		node *child = tree[edges[i].first];
+		node *parent = tree[edges[i].second];
+		parent->children.push_back(child);
+		child->parent = parent;
+	}
 
-	const int ret = count_cuts(&tree[0]);
+	trace(tree);
+
+	calc_rank(tree[0]);
+
+	trace(tree);
+
+	const int ret = count_cuts(tree[0]);
+
+	trace(tree);
 
 	cout << ret << endl;
 
-	delete [] tree;
+	for (int i = 0; i < N; i++)
+		delete tree[i];
+
 	return 0;
 }
